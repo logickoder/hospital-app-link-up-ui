80a80c40b9eb1bed0ad57968d0fa335a4ab81ce1
logickoder <chukwudumebiorazulike@gmail.com>
2023-04-01 19:17:47 +0100

    chore: created custom  input

--- lib/appointment/create_appointment_screen.dart
+++ lib/appointment/create_appointment_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../common/resources.dart';
+import '../common/widgets/input.dart';
 import '../common/widgets/top_bar.dart';
 import 'appointment_tile.dart';
 
@@ -14,6 +16,30 @@ class CreateAppointmentScreen extends StatelessWidget {
         shrinkWrap: true,
         children: [
           AppointmentTiles(),
+          Padding(
+            padding: const EdgeInsets.all(AppPadding.normal),
+            child: Column(
+              children: [
+                Row(
+                  children: const [
+                    Expanded(
+                      child: Input(
+                        label: 'Select time',
+                        trailing: Icon(Icons.access_time_outlined),
+                      ),
+                    ),
+                    SizedBox(width: AppPadding.medium),
+                    Expanded(
+                      child: Input(
+                        label: 'Select date',
+                        trailing: Icon(Icons.calendar_month_outlined),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
         ],
       ),
     );
--- lib/common/widgets/input.dart
+++ lib/common/widgets/input.dart
@@ -37,82 +37,105 @@ class _InputState extends ConsumerState<Input> {
   final _focused = StateProvider((ref) => false);
   final _inputFocus = FocusNode();
 
+  void _setFocus(bool value) => ref.read(_focused.notifier).state = value;
+
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
     return FocusScope(
       child: Focus(
-        onFocusChange: (value) {
-          ref.read(_focused.notifier).state = value;
-        },
+        onFocusChange: _setFocus,
         child: GestureDetector(
-          onTap: () => FocusScope.of(context).requestFocus(_inputFocus),
+          onTap: () {
+            _setFocus(true);
+            FocusScope.of(context).requestFocus(_inputFocus);
+          },
           child: FormField(
             validator: widget.validator,
             initialValue: widget.controller?.text,
             builder: (state) {
-              return Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    padding: widget.padding,
-                    decoration: BoxDecoration(
-                      color: theme.colorScheme.onPrimary,
-                      border: Border.all(
-                        color: ref.watch(_focused)
-                            ? theme.textTheme.bodyMedium?.color ?? Colors.black
-                            : const Color(0xFF96A3B1),
-                      ),
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: Row(
-                      children: [
-                        if (widget.leading != null) widget.leading!,
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              if (widget.label != null)
-                                Text(
-                                  widget.label!,
-                                  style: theme.textTheme.labelSmall,
-                                ),
-                              TextField(
-                                focusNode: _inputFocus,
-                                controller: widget.controller,
-                                keyboardType: widget.keyboardType,
-                                obscureText: widget.obscureInput,
-                                onChanged: (value) => state.didChange(value),
-                                decoration: const InputDecoration(
-                                  isDense: true,
-                                  border: InputBorder.none,
-                                  contentPadding: EdgeInsets.zero,
-                                  counterText: '',
-                                ),
-                                style: widget.style,
-                                maxLength: widget.maxLength,
-                              ),
-                            ],
-                          ),
+              final isFocused = ref.watch(_focused);
+
+              return LayoutBuilder(builder: (context, constraints) {
+                return Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      padding: widget.padding,
+                      decoration: BoxDecoration(
+                        color: theme.colorScheme.onPrimary,
+                        border: Border.all(
+                          color: isFocused
+                              ? theme.textTheme.bodyMedium?.color ??
+                                  Colors.black
+                              : const Color(0xFF96A3B1),
                         ),
-                        if (widget.trailing != null) widget.trailing!,
-                      ],
-                    ),
-                  ),
-                  if (state.hasError) ...{
-                    const SizedBox(height: AppPadding.small / 2),
-                    Text(
-                      state.errorText!,
-                      style: theme.textTheme.labelSmall?.copyWith(
-                        fontWeight: FontWeight.w500,
-                        color: theme.colorScheme.error,
+                        borderRadius: BorderRadius.circular(8),
                       ),
-                    )
-                  },
-                ],
-              );
+                      child: Row(
+                        children: [
+                          if (widget.leading != null) widget.leading!,
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                if (widget.label != null) ...{
+                                  Transform.translate(
+                                    offset: Offset(
+                                      0,
+                                      (isFocused ||
+                                              state.value?.isNotEmpty == true)
+                                          ? 0
+                                          : 12,
+                                    ),
+                                    child: Text(
+                                      widget.label!,
+                                      style:
+                                          theme.textTheme.labelSmall?.copyWith(
+                                        fontFamily: isFocused
+                                            ? AppFonts.titleFamily
+                                            : null,
+                                      ),
+                                    ),
+                                  )
+                                },
+                                TextField(
+                                  focusNode: _inputFocus,
+                                  controller: widget.controller,
+                                  keyboardType: widget.keyboardType,
+                                  obscureText: widget.obscureInput,
+                                  onChanged: (value) => state.didChange(value),
+                                  decoration: const InputDecoration(
+                                    isDense: true,
+                                    border: InputBorder.none,
+                                    contentPadding: EdgeInsets.zero,
+                                    counterText: '',
+                                  ),
+                                  style: widget.style,
+                                  maxLength: widget.maxLength,
+                                ),
+                              ],
+                            ),
+                          ),
+                          if (widget.trailing != null) widget.trailing!,
+                        ],
+                      ),
+                    ),
+                    if (state.hasError) ...{
+                      const SizedBox(height: AppPadding.small / 2),
+                      Text(
+                        state.errorText!,
+                        style: theme.textTheme.labelSmall?.copyWith(
+                          fontWeight: FontWeight.w500,
+                          color: theme.colorScheme.error,
+                        ),
+                      )
+                    },
+                  ],
+                );
+              });
             },
           ),
         ),
